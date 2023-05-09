e7168a6d4f3a38ce92a1d4da1f1c05876440d9db
lindabalode <balode.lindaa@gmail.com>
2023-05-09 11:07:04 +0300

    changes

--- app/ApiClient.php
+++ app/ApiClient.php
@@ -11,7 +11,7 @@ class ApiClient
 {
 
     private Client $client;
-    private const URL = "https://rickandmortyapi.com/api/character/?page=9";
+    private const URL = "https://rickandmortyapi.com/api/character/?page=8";
 
     public function __construct()
     {
@@ -20,11 +20,18 @@ class ApiClient
 
     public function fetchCharacters(): array
     {
-        $characterCollection = [];
-        $response = $this->client->get(self::URL);
-        $characters = json_decode($response->getBody()->getContents());
-        foreach ($characters->results as $character) {
-            $episode = json_decode($this->client->get($character->episode[0])->getBody()->getContents());
+        try {
+            $characterCollection = [];
+            if (!Cache::ifHas('characters')) {
+                $response = $this->client->get(self::URL);
+                $responseJson = $response->getBody()->getContents();
+                Cache::save('characters', $responseJson, 120);
+            } else {
+                $responseJson = Cache::get('characters');
+            }
+            $characters = json_decode($responseJson);
+            foreach ($characters->results as $character) {
+                $episode = json_decode($this->client->get($character->episode[0])->getBody()->getContents());
                 $characterCollection[] = new Character
                 (
                     $character->name,
@@ -35,7 +42,10 @@ class ApiClient
                     $character->image
                 );
             }
-        return $characterCollection;
+            return $characterCollection;
+        } catch (GuzzleException $e) {
+            return [];
+        }
 
     }
 
--- app/Cache.php
+++ app/Cache.php
@@ -2,7 +2,37 @@
 
 namespace RickMortyApi;
 
+use Carbon\Carbon;
+
 class Cache
 {
+    public static function save(string $key, string $data, int $ttl = 120): void
+    {
+        $cacheFile = '../cache/' . $key;
+        file_put_contents($cacheFile, json_encode([
+            'expires_at' => Carbon::now()->addSecond($ttl),
+            'content' => $data
+        ]));
+    }
 
+    public static function delete(string $key): void
+    {
+        unlink('cache/' . $key);
+    }
+
+    public static function ifHas(string $key): bool
+    {
+        if (!file_exists('cache/' . $key))
+        {
+            return false;
+        }
+        $content = json_decode(file_get_contents('cache/' . $key));
+        return Carbon::now() < $content->expires_at;
+    }
+
+    public static function get(string $key): string
+    {
+        $content = json_decode(file_get_contents('cache/' . $key));
+        return $content->content;
+    }
 }
